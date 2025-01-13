c4020f2127f099165d46e4b18ab27796410f9845
m2palomo <m2palomov@gmail.com>
2025-01-13 09:17:54 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    var member = this;
+    member.skills = [
+        {name: 'JavaScript', level: 3},
+        {name: 'HTML', level: 2},
+        {name: 'CSS', level: 2},
+        {name: 'Node.js', level: 1},
+        {name: 'React', level: 1}
+    ];
+}
